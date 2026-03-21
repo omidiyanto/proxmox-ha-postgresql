28cafef8fd3907c22f363ede03dbf73bc636a5a8
O Midiyanto <o.midiyanto@satnusa.com>
2026-03-22 00:49:25 +0700

    feat: SWITCHOVER

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -4,3 +4,8 @@ output "cluster_nodes" {
     for k, node in module.pg_cluster : k => node.vm_ipv4_address
   }
 }
+output "ansible_inventory_raw" {
+  description = "Raw YAML Inventory for Ansible
+  value       = local_file.ansible_inventory.content
+  sensitive   = true
+}
