4709ed79370337b0cc54ae8ce4aeea1e341d50f1
O Midiyanto <o.midiyanto@satnusa.com>
2026-03-21 12:09:16 +0700

    fix cicd

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,7 @@
 variable "proxmox_api_url" { type = string }
 variable "proxmox_token_id" { type = string }
-variable "proxmox_token_secret" { type = string, sensitive = true }
+variable "proxmox_token_secret" { 
+  type      = string
+  sensitive = true 
+}
 variable "ssh_public_key" { type = string }
